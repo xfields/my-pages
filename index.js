a953f24f595b153b96a9327d97e301acf5d2bae8
辛佰惠 <xinbaihui@xinbaihuideMacBook-Air.local>
2017-03-26 13:45:47 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const serve = require('koa-static');
+const Koa = require('koa');
+const app = new Koa();
+const port = process.env.NODE_ENV === 'development' ? 3000 : 80
+
+app.use(serve('regexper'));
+app.use(serve('uidocs'));
+app.listen(port);
+
+console.log(`listening on port ${port}`);
